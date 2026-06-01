ed886a9ef3402ea0177bfc1fa6b18867292f4374
Jovan Veljanoski <veljanoski.jovan100@gmail.com>
2026-06-01 20:14:16 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,12 +8,22 @@ const rl = createInterface({
 
 rl.prompt();
 
+const BUILT_IN_COMMANDS = ["exit", "type", "echo"];
+
 rl.on("line", (command) => {
   if (command === "exit") {
     rl.close();
     return;
   } else if (command.startsWith("echo ")) {
     console.log(command.slice(5));
+  } else if (command.startsWith("type ")) {
+    const argsAfterType = command.slice(5);
+
+    if (BUILT_IN_COMMANDS.includes(argsAfterType)) {
+      console.log(`${argsAfterType} is a shell builtin`);
+    } else {
+      console.log(`${argsAfterType}: not found`);
+    }
   } else {
     console.log(`${command}: command not found`);
   }

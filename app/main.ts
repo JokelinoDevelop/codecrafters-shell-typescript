7532c0e83772500e31c41237a3f9231db8673a68
Jovan Veljanoski <jokac@Jovans-MacBook-Pro.local>
2026-06-07 18:59:49 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,7 @@ import { createInterface } from "readline";
 import { constants } from "fs/promises";
 import path from "path";
 import { accessSync } from "fs";
+import { execFile } from "node:child_process";
 
 const rl = createInterface({
   input: process.stdin,
@@ -62,6 +63,43 @@ rl.on("line", (input) => {
   if (commands[command]) {
     commands[command](...args);
   } else {
+    // check if command is executable
+    const dirPath = process.env.PATH;
+
+    if (!dirPath) return "Please provide a PATH env variable!";
+
+    const directories = dirPath
+      .split(path.delimiter)
+      .map((item) => item.trim());
+
+    let foundPath: string | null = null;
+
+    for (const dir of directories) {
+      const fullPath = path.join(dir, command);
+
+      try {
+        accessSync(fullPath, constants.X_OK);
+
+        foundPath = fullPath;
+      } catch (err) {
+        // Doesn't exist or its not executable, continue!
+      }
+    }
+
+    // we found executable file
+    if (foundPath) {
+      // execute the program
+      execFile(foundPath, [...args], (error, stdout) => {
+        if (error) {
+          console.log(error.message);
+          return;
+        }
+
+        console.log(stdout);
+      });
+      return;
+    }
+
     console.log(`${command}: command not found`);
   }
 
